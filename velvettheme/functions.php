f5a94e07742b3de813f311a1cd9a517a385c4640
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-06-30 14:43:13 +0530

    Add files via upload

--- velvettheme/functions.php
+++ velvettheme/functions.php
@@ -43,17 +43,37 @@ function velvet_custom_post_type() {
 add_action('init', 'velvet_custom_post_type');
 
 
-// function velvet_add_custom_box() {
-//   $screens = [ 'post', 'books' ];
-//   foreach ( $screens as $screen ) {
-//       add_meta_box(
-//           'author_box_id',                 // Unique ID
-//           'Author',      // Box title
-//           'author_custom_box_html',  // Content callback, must be of type callable
-//           $screen                            // Post type
-//       );
-//   }
-// }
-// add_action( 'add_meta_boxes', 'velvet_add_custom_box' );
+function velvet_add_custom_box() {
+  $screens = [ 'post', 'books' ];
+  foreach ( $screens as $screen ) {
+      add_meta_box(
+          'author_box_id',                 // Unique ID
+          'Author',      // Box title
+          'author_custom_box_html',  // Content callback, must be of type callable
+          $screen                            // Post type
+      );
+  }
+}
+add_action( 'add_meta_boxes', 'velvet_add_custom_box' );
+
+function author_custom_box_html( $post ) {
+  $value = get_post_meta( $post->ID, '_velvet_meta_key', true );
+  ?>
+    <label for="author_field">Description for this field</label>
+    <input type="text" id="author_field" name="author_field"  value="<?php _e($value , 'velvettheme' ); ?>" >
+  <?php
+}
 
 
+function velvet_save_postdata( $post_id ) {
+  $title = sanitize_text_field( $_POST['author_field'] );
+
+  if ( array_key_exists( 'author_field', $_POST ) ) {
+      update_post_meta(
+          $post_id,
+          '_velvet_meta_key',
+          $_POST['author_field']
+      );
+  }
+}
+add_action( 'save_post', 'velvet_save_postdata' );
--- velvettheme/single.php
+++ velvettheme/single.php
@@ -14,6 +14,9 @@ get_header(); ?>
      <?php the_title(); ?>
     </h1>
     <p><?php echo get_the_content(); ?></p>
+    <?php  $value = get_post_meta( $post->ID, '_velvet_meta_key', true );
+    echo "<h5> $value </h5>";
+    ?>
    <?php endwhile;
 else :
     _e( 'Sorry, no posts were found.', 'velvettheme' );
