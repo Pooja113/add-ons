afd3f75b6c0de51d10d4657714137961b46ea27e
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-07-01 22:18:51 +0530

    Add files via upload

--- velvet/admin/view.php
+++ velvet/admin/view.php
@@ -2,15 +2,25 @@
  if ( ! current_user_can( 'manage_options' ) ) {
   return;
 }
+
+if ( isset( $_GET['settings-updated'] ) ) {
+  add_settings_error( 'wporg_messages', 'wporg_message', __( 'Settings Saved', 'wporg' ), 'updated' );
+}
+
+settings_errors( 'wporg_messages' );
 ?>
 
 <div class="wrap">
     <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
-    <form action="<?php menu_page_url( 'velvetop' ) ?>" method="post">
+    <form action="options.php" method="post">
           <?php
-      settings_fields( 'velvet_options' );
+      settings_fields( 'velvetop' );
       do_settings_sections( 'velvetop' );
       submit_button( __( 'Save Settings', 'textdomain' ) );
       ?>
     </form>
   </div>
+
+
+
+  
